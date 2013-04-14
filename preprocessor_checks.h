39672583fcb9e12f6bda15f02a96ecb939d41a31
Fletcher Porter <fp.monkey@gmail.com>
2013-04-14 10:23:32 -0700

    added some preprocessor stuff

---
+++ preprocessor_checks.h
@@ -0,0 +1,34 @@
+//
+// preprocessor_checks.h
+//
+// Copyright (c) Fletcher Porter 2013
+//
+
+#ifndef ROBOTC
+#error You need to compile this in RobotC
+#endif
+
+#ifndef NXT
+#warning You do not currently have NXT mode active on your compiler
+#endif
+
+#ifndef TETRIX
+#warning You do not currently have Tetrix mode active on your compiler
+#endif
+
+#ifndef FTC
+#warning You do not currently have FTC mode active on your compiler
+#endif
+
+#if firmwareVersion < 0 // TODO: change 0 to latest firmware verison
+#warning You do not currently have the latest firmware
+#endif
+
+#define PREPROCESSOR_CHECKS_EXECUTED
+// You can use this to only run your program if the checks have gon through
+// ex: 
+// #ifdef preprocessor_checks_executed
+// run_the_program();
+// #else
+// dont_run_the_program();
+// #endif
