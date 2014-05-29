7296de93f99774b5e08550e1eb64c040a49083c0
Fletcher Porter <fp.monkey@gmail.com>
2014-05-28 17:47:01 -0700

    fixed vector.h so all functions work

--- vector.h
+++ vector.h
@@ -4,36 +4,30 @@
 // Copyright (c) 2013 Suit Bots 4628
 //
 
-long magnitude(vect vector) {
-  long mag;
-  mag += vector.x * vector.x + vector.y * vector.y + vector.z * vector.z;
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct {
+  double x;
+  double y;
+  double z;
+} vect;
+
+double magnitude(vect vector) {
+  double mag;
+  mag = vector.x * vector.x + vector.y * vector.y + vector.z * vector.z;
   mag = sqrt(mag);
   return mag;
 }
 
-int directionXY(vect vector) {
-  int m = vector.y / vector.x;
-  if (vect.x < 0) 
-    m = atan(m) + 180;
-  else if (m == nan && vect.y > 0)
-    m = 90;
-  else if (m == nan && vect.y < 0)
-    m = 270;
-  else
-    m = atan(m);
+double directionXY(vect vector) { //angle in degrees
+  double m = atan2(vector.y, vector.x) * 180 / M_PI;
   return m;
 }
 
-int directionXZ(vect vector) {
-  int m = vector.z / vector.x;
-  if (vect.x < 0) 
-    m = atan(m) + 180;
-  else if (m == nan && vect.z > 0)
-    m = 90;
-  else if (m == nan && vect.z < 0)
-    m = 270;
-  else
-    m = atan(m);
+double directionXZ(vect vector) { //angle in degrees
+  double m = atan2(vector.z, vector.x) * 180 / M_PI;
   return m;
 }
 
@@ -42,14 +36,14 @@ vect add(vect vect1, vect vect2) {
   return vector;
 }
 
-vect mult(long scalar, vect vector) {
-  vect vector = { vector.x * scalar, vector.y * scalar, vector.z * scalar };
-  return vector;
+vect mult(double scalar, vect vector) {
+  vect vector_f = { vector.x * scalar, vector.y * scalar, vector.z * scalar };
+  return vector_f;
 }
 
-vect dot_prod(vect vect1, vect vect2) {
-  vect vector = { vect1.x * vect2.x, vect1.y * vect2.y, vect1.z * vect2.z };
-  return vector;
+double dot_prod(vect vect1, vect vect2) {
+  double prod = vect1.x * vect2.x + vect1.y * vect2.y + vect1.z * vect2.z;
+  return prod;
 }
 
 vect unit_vector(vect vector) {
@@ -57,15 +51,29 @@ vect unit_vector(vect vector) {
 }
 
 bool equivalent(vect vect1, vect vect2) {
-  if (! magnitude(vect1) == magnitude(vect2))
+  if (magnitude(vect1) != magnitude(vect2))
     return false;
-  if (! directionXY(vect1) == directionXY(vect2))
+  if (directionXY(vect1) != directionXY(vect2))
     return false;
-  if (! directionXZ(vect1) == directionXZ(vect2))
+  if (directionXZ(vect1) != directionXZ(vect2))
     return false;
   return true;
 }
 
-void useless_val_warning(long useless_val) {
-  write_debug_stream("WARNING: %ld has no meaning", useless_val);
+void useless_val_warning(double useless_val) {
+  // write_debug_stream("WARNING: %f has no meaning", useless_val);
+  printf ("WARNING: %f has no meaning", useless_val);
 }
+
+
+/*  ____        _ _     ____        _         _____ _____ ____  */
+/* / ___| _   _(_) |_  | __ )  ___ | |_ ___  |  ___|_   _/ ___| */
+/* \___ \| | | | | __| |  _ \ / _ \| __/ __| | |_    | || |     */
+/*  ___) | |_| | | |_  | |_) | (_) | |_\__ \ |  _|   | || |___  */
+/* |____/ \__,_|_|\__| |____/ \___/ \__|___/ |_|     |_| \____| */
+                                                            
+/*  _  _    __  ____  ___   */
+/* | || |  / /_|___ \( _ )  */
+/* | || |_| '_ \ __) / _ \  */
+/* |__   _| (_) / __/ (_) | */
+/*    |_|  \___/_____\___/  */
